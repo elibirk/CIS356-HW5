6d6138ed796965069732a789563d0131377a2be0
Leah Perry <perryleahn@gmail.com>
2018-03-05 19:55:19 -0600

    Update sec5.sh

--- sec5.sh
+++ sec5.sh
@@ -15,5 +15,5 @@ cd /home/CIS356
 sudo mkdir -p $name
 sudo chown -R $name:$name /home/CIS356/$name
 sudo cp /etc/skel/.bash_logout /home/CIS356/$name/.bash_logout
-sudo cp /etc/skel/.bashrc /home/CIS356/$name/.bashrx
+sudo cp /etc/skel/.bashrc /home/CIS356/$name/.bashrc
 sudo cp /etc/skel/.profile /home/CIS356/$name/.profile
