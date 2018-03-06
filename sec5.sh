bc2bf2a607152088210cb110c6291d5e6b2d9d64
Leah Perry <perryleahn@gmail.com>
2018-03-05 19:51:12 -0600

    Add files via upload

---
+++ sec5.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+name=$1
+
+cd /home/users
+sudo mkdir -p $name
+sudo chown -R $name:$name /home/users/$name
+sudo cp /etc/skel/.bash_logout /home/users/$name/.bash_logout
+sudo cp /etc/skel/.bashrc /home/users/$name/.bashrc
+sudo cp /etc/skel/.profile /home/users/$name/.profile
+
+cd /home/CIS356
+
+sudo mkdir -p $name
+sudo chown -R $name:$name /home/CIS356/$name
+sudo cp /etc/skel/.bash_logout /home/CIS356/$name/.bash_logout
+sudo cp /etc/skel/.bashrc /home/CIS356/$name/.bashrx
+sudo cp /etc/skel/.profile /home/CIS356/$name/.profile
